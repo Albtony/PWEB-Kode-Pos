e360981ebe25cff6d2fdecd1eb476b0a1091bd7a
albtony <albtony21@gmail.com>
2022-09-26 21:01:14 +0700

    80% progress done

---
+++ index.js
@@ -0,0 +1,24 @@
+import { post_code } from './postal_array.js'
+
+window.test = () => {
+    var postalCode = document.getElementById("postal-code").value;
+    if (postalCode == "" || postalCode == null) return;
+    
+    const postalResult = post_code.filter((item) => {
+        return item["postal_code"] == postalCode;
+    });
+
+    if (postalResult.length == 0) {
+        alert("No result was found");
+        return;
+    }
+
+    var list = document.createElement('ul');
+    for (const elem of postalResult) {
+        let item = document.createElement('li');
+        let text = elem["postal_code"] + " - " + elem["city"] + ", " + elem["sub_district"]  + ", "
+        item.appendChild(document.createTextNode(text));
+        list.appendChild(item);
+    }
+    document.getElementById('result').appendChild(list)
+}
